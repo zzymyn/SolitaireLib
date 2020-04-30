f4634593f79064463d7625a0acadebb505020c17
Ivan Neeson <zzymyn@gmail.com>
2020-04-30 21:40:39 +1000

    Fix default values for options.

--- CardLib/Model/GameOptionsBase.ts
+++ CardLib/Model/GameOptionsBase.ts
@@ -6,24 +6,30 @@ export class GameOptionsBase {
     }
 
     protected getFloat_(key: string, defaultValue: number) {
-        const value = Number(this.params_.get(key));
+        const valueStr = this.params_.get(key);
+        if (valueStr === undefined || valueStr === null || valueStr === "")
+            return defaultValue;
+        const value = Number(valueStr);
         if (Number.isNaN(value))
             return defaultValue;
         return value;
     }
 
     protected getInt_(key: string, defaultValue: number) {
-        const value = Number(this.params_.get(key));
+        const valueStr = this.params_.get(key);
+        if (valueStr === undefined || valueStr === null || valueStr === "")
+            return defaultValue;
+        const value = Number(valueStr);
         if (Number.isNaN(value) || !Number.isInteger(value))
             return defaultValue;
         return value;
     }
 
     protected getBool_(key: string, defaultValue: boolean) {
-        const value = this.params_.get(key);
-        if (value === undefined || value === null)
+        const valueStr = this.params_.get(key);
+        if (valueStr === undefined || valueStr === null)
             return defaultValue;
-        if (value === "")
+        if (valueStr === "")
             return true;
         if (this.params_.get(key))
             return true;
